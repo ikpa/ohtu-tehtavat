d23253b09f754844844cc5c37ca38d72c4994e68
Patrik Ikäheimonen <patrik.ikaheimonen@helsinki.fi>
2020-11-12 16:59:02 +0200

    viikko 3 tehtävät 1-3

--- viikko3/nhlreader/src/main/java/ohtu/Player.java
+++ viikko3/nhlreader/src/main/java/ohtu/Player.java
@@ -1,8 +1,48 @@
 
 package ohtu;
 
-public class Player {
+public class Player implements Comparable {
     private String name;
+    private int assists;
+    private int goals;
+    private String team;
+    private String nationality;
+    
+    public int score() {
+        return this.assists + this.goals;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setAssists(int assists) {
+        this.assists = assists;
+    }
+
+    public void setGoals(int goals) {
+        this.goals = goals;
+    }
+
+    public int getAssists() {
+        return assists;
+    }
+
+    public int getGoals() {
+        return goals;
+    }
 
     public void setName(String name) {
         this.name = name;
@@ -13,8 +53,15 @@ public class Player {
     }
 
     @Override
+    public int compareTo(Object o) {
+        return ((Player) o).score() - this.score();
+    }
+    
+    @Override
     public String toString() {
-        return name;
+        return name + ", team: " + team + 
+                ", goals: " + goals + ", assists: " + assists +
+                ", score: " + this.score();
     }
       
 }
